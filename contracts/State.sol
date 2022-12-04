e106c1c74f608d6f2bfae8b823466833a57e9ed2
salil-naik <salil.naik27@gmail.com>
2022-12-05 01:15:22 +0530

    feat: added spokePool deposit function and replicated mapping for other types

--- contracts/State.sol
+++ contracts/State.sol
@@ -1,12 +1,22 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identelse ifier: UNLICENSED
 pragma solidity 0.8.17;
 
 import "./interfaces/IPlug.sol";
 import "./interfaces/ISocket.sol";
 import "./utils/Ownable.sol";
 
-contract State is Ownable, IPlug {
+interface SpokePool {
+    function deposit(
+        address recipient,
+        address originToken,
+        uint256 amount,
+        uint256 destinationChainId,
+        uint64 relayerFeePct,
+        uint32 quoteTimestamp
+    ) external payable;
+}
 
+contract State is Ownable, IPlug {
     enum UpdateType {
         ADDRESS,
         UINT8,
@@ -15,7 +25,8 @@ contract State is Ownable, IPlug {
         BOOL,
         BYTES,
         BYTES4,
-        BYTES32
+        BYTES32,
+        WITHDRAW
     }
 
     struct StateUpdate {
@@ -45,10 +56,10 @@ contract State is Ownable, IPlug {
         _socket__ = ISocket(socket_);
     }
 
-    function configure(
-        uint256 controllerChainSlug_,
-        address controllerAddress_
-    ) external onlyOwner {
+    function configure(uint256 controllerChainSlug_, address controllerAddress_)
+        external
+        onlyOwner
+    {
         _socket__.setPlugConfig(
             controllerChainSlug_,
             controllerAddress_,
@@ -56,18 +67,65 @@ contract State is Ownable, IPlug {
         );
     }
 
+    SpokePool public _spokePool;
+    uint8 public _relayerFeePct = 1;
+
+    function updateSpokePool(uint8 value_) external {
+        _relayerFeePct = value_;
+    }
+
     // TODO: extend for withdrawing liquidity
     function inbound(bytes calldata payload_) external payable {
         if (msg.sender != address(_socket__)) revert NotSocket();
-        (StateUpdate[] memory updates) = abi.decode(payload_, (StateUpdate[]));
+        StateUpdate[] memory updates = abi.decode(payload_, (StateUpdate[]));
         uint256 len = updates.length;
         for (uint256 i = 0; i < len; i++) {
             StateUpdate memory update = updates[i];
             if (update.updateType == UpdateType.ADDRESS) {
-                (address value) = abi.decode(update.value, (address));
+                address value = abi.decode(update.value, (address));
                 _addressMap[update.variable] = value;
+            } else if (update.updateType == UpdateType.UINT8) {
+                uint8 value = abi.decode(update.value, (uint8));
+                _uint8Map[update.variable] = value;
+            } else if (update.updateType == UpdateType.UINT64) {
+                uint64 value = abi.decode(update.value, (uint64));
+                _uint64Map[update.variable] = value;
+            } else if (update.updateType == UpdateType.UINT256) {
+                uint256 value = abi.decode(update.value, (uint256));
+                _uint256Map[update.variable] = value;
+            } else if (update.updateType == UpdateType.BOOL) {
+                bool value = abi.decode(update.value, (bool));
+                _boolMap[update.variable] = value;
+            } else if (update.updateType == UpdateType.BYTES) {
+                bytes memory value = abi.decode(update.value, (bytes));
+                _bytesMap[update.variable] = value;
+            } else if (update.updateType == UpdateType.BYTES4) {
+                bytes4 value = abi.decode(update.value, (bytes4));
+                _bytes4Map[update.variable] = value;
+            } else if (update.updateType == UpdateType.BYTES32) {
+                bytes32 value = abi.decode(update.value, (bytes32));
+                _bytes32Map[update.variable] = value;
+            } else if (update.updateType == UpdateType.WITHDRAW) {
+                (
+                    address _userAddress,
+                    address _tokenAddress,
+                    uint256 _amount,
+                    uint8 _chainId
+                ) = abi.decode(
+                        update.value,
+                        (address, address, uint256, uint8)
+                    );
+                uint32 _timeStamp = uint32(block.timestamp);
+
+                _spokePool.deposit(
+                    _userAddress,
+                    _tokenAddress,
+                    _amount,
+                    _chainId,
+                    _relayerFeePct,
+                    _timeStamp
+                );
             }
-            // TODO: replicate for other types
         }
     }
 }
